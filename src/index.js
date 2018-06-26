f8b1f7ff4179ec914bf0308af5ad234ddf8c5100
Nuno Neves <youknownuno@gmail.com>
2018-06-26 13:04:07 +0100

    implemented localStorage for caching bookmarks on the browser

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const list = document.getElementById('bmk-list')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 
-// react in a nutshell
+// on load look for state on localstorage, otherwise create an empty one
+
 const store = {
-	state : {
+	state : JSON.parse(localStorage.getItem('bm_store')) || {
 		count: 1,
 		pages: {
 			1: [],
@@ -21,7 +22,11 @@ const store = {
 	setState: function(newState) {
 		//didUpdate
 		this.state = Object.assign( {}, this.state, newState )
-		
+		localStorage.setItem('bm_store', JSON.stringify(this.state));
+		// display:
+			// thank you message
+			// link submitted
+			// link back to home page
 	},
 
 	addURL: function(url) {
@@ -41,12 +46,46 @@ const store = {
 		pages[count] = last
 		this.setState({ count: count, pages : pages })
 		
+		// change location of this call to didUpdate
+		this.renderList(this.state.pages[this.state.selected])
 		console.log(store)
+	},
+
+	didUpdate: function(newState) {
+		// check if parts of the state have been changed
+			// re-render components that have been changed
+	},
+
+	renderList : function() {
+		while ( list.firstChild ) {
+			list.removeChild(list.firstChild)
+		}
+
+		const newList = this.state.pages[this.state.selected]
+
+		for(let i = 0; i < newList.length; i++) {
+			let item = document.createElement('ul')
+			let link = document.createElement('a')
+
+			let del = document.createElement('button')
+			del.innerHTML = ('delete')
+			del.addEventListener('click', () => console.log(del) )
+
+			let edit = document.createElement('button')
+			edit.innerHTML = ('edit')
+			edit.addEventListener('click', () => console.log(edit) )
+
+			link.href = newList[i]
+			link.innerHTML = newList[i]
+
+			item.appendChild(link)
+			item.appendChild(edit)
+			item.appendChild(del)
+
+			list.appendChild(item)
+		}
 	}
 
-	didUpdate: function() {
-		// trigger a re-render here
-	}
 
 }
 
@@ -54,3 +93,9 @@ const store = {
 submit.addEventListener('click', () => { store.addURL(formInput.value)})
 prev.addEventListener('click', () => console.log('prev'))
 next.addEventListener('click', () => console.log('next'))
+
+store.renderList();
+
+// minify app with google closure compiler
+// advanced optimizations
+// deploy app
